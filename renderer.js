f92a18484e0fa88caf423da5e74bf6b37bc33369
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-08-13 16:31:46 +0700

    handle select option items

--- renderer.js
+++ renderer.js
@@ -1,3 +1,4 @@
+// handle select folder
 const selectFolderBtn = document.querySelector(".btn__folder--select");
 const filePathElement = document.getElementById("folder__input");
 
@@ -10,3 +11,43 @@ const singleVideoElement = document.getElementById(
     "box__content__single-video"
 );
 const multiVideoElement = document.getElementById("box__content__multie-video");
+
+// handle select dropdown menu
+const dropdownElement = document.querySelector(".dropdown ");
+
+dropdownElement.onmouseover = function () {
+    if (!dropdownElement.classList.contains("is-active")) {
+        dropdownElement.classList.add("is-active");
+    }
+};
+
+dropdownElement.onmouseout = function () {
+    if (dropdownElement.classList.contains("is-active")) {
+        dropdownElement.classList.remove("is-active");
+    }
+};
+
+const listOptionItem = document.querySelectorAll(
+    ".option__dropdown-menu .dropdown-item"
+);
+
+const boxContentSingleVideoElement = document.getElementById(
+    "box__content__single-video"
+);
+
+const boxContentMultiVideoElement = document.getElementById(
+    "box__content__multi-video"
+);
+
+listOptionItem.forEach((dropItem) => {
+    dropItem.onclick = function () {
+        if (this.classList.contains("select__single-video")) {
+            boxContentSingleVideoElement.classList.remove("is-hidden");
+            boxContentMultiVideoElement.classList.add("is-hidden");
+        }
+        if (this.classList.contains("select__multi-video")) {
+            boxContentSingleVideoElement.classList.add("is-hidden");
+            boxContentMultiVideoElement.classList.remove("is-hidden");
+        }
+    };
+});
