2827f2b47999a6090bf93bd3d38f7891dc7cc1fe
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-08-13 10:13:11 +0700

    build ui download multi video layout

--- renderer.js
+++ renderer.js
@@ -5,3 +5,8 @@ selectFolderBtn.addEventListener("click", async () => {
     const filePath = await window.test.openFile();
     filePathElement.value = filePath;
 });
+
+const singleVideoElement = document.getElementById(
+    "box__content__single-video"
+);
+const multiVideoElement = document.getElementById("box__content__multie-video");
